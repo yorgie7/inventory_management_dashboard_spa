141a5dd63984f3ed0967a91fdc076e187c5d85cc
Yogesh Bamanier <bamanieryogesh1@gmail.com>
2025-04-05 16:41:38 +0530

    Update ErrorBoundary.js

--- src/utils/ErrorBoundary.js
+++ src/utils/ErrorBoundary.js
@@ -9,7 +9,6 @@ export default class ErrorBoundary extends React.Component {
 
     componentDidCatch(error, errorInfo) {
         // Catch errors in any components below and re-render with error message
-            
         this.setState({
             error: error,
             errorInfo: errorInfo,
